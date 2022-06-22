eddfbff605270ddd5fd91701198d9927fd94efee
ejb1733 <ethanjbillings@gmail.com>
2022-06-21 19:09:08 -0700

    Add logic to to reset game with 1000ms delay

--- app.js
+++ app.js
@@ -1,123 +1,142 @@
 const gbdiv = document.querySelector('#gameboard');
 const body = document.querySelector('body');
+const restartbtn = document.createElement('button');
+
+restartbtn.innerText = 'reset';
+restartbtn.setAttribute('id', 'restartbtn');
+restartbtn.addEventListener('click', () => {
+  while (gbdiv.firstChild) {
+    gbdiv.removeChild(gbdiv.firstChild);
+  }
+  displayController.numPlays = 0;
+  gameBoard.gameboard = ['', '', '', '', '', '', '', '', ''];
+  setTimeout(() => {
+    gameBoard.setup();
+  }, 1000);
+});
 
 const gameBoard = (() => {
-    let gameboard = ['', '', '', 
-                     '', '', '',
-                     '', '', '',];
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
 
-    const setup = () => {
-        for (let i = 0; i < 9; i++) {
-            const square = document.createElement('div');
-            square.setAttribute('class', 'empty');
-            square.setAttribute('id', `sq${i}`);
-            square.setAttribute('onclick', `displayController.render(${i})`);
-            gbdiv.appendChild(square);
-            // console.log('render empty square');
-        }
+  const setup = () => {
+    for (let i = 0; i < 9; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('class', 'empty');
+      square.setAttribute('id', `sq${i}`);
+      square.setAttribute('onclick', `displayController.render(${i})`);
+      gbdiv.appendChild(square);
     }
+    gbdiv.appendChild(restartbtn);
+    restartbtn.style.display = 'none';
+    console.log('setup board successfully');
+  };
 
-    return {
-        gameboard,
-        setup,
-    };
+  return {
+    gameboard,
+    setup,
+  };
 })();
 
 const player = (name, choice) => {
-    return {name, choice};
-}
+  return { name, choice };
+};
 
 const player1 = player('me', 'x');
 const player2 = player('they', 'o');
 
-const players = [
-    player1,
-    player2,
-]
+const players = [player1, player2];
 
 let currPlayer = players[0];
 
 const displayController = (() => {
-    let numPlays = 0;
+  let numPlays = 0;
+  let winMessage = '';
 
-    const render = (index) => {
-        let toChange = document.querySelector(`#sq${index}`);
-        if (toChange.innerText === '') {
-            numPlays++;
-            gameBoard.gameboard[index] = currPlayer.choice;
-            toChange.innerText = gameBoard.gameboard[index];
-            // console.log(`rendered an ${gameBoard.gameboard[index]} at ${index}`);
-            currPlayer === players[0] ? currPlayer = players[1] : currPlayer = players[0];
-            isGameOver();
+  const render = (index) => {
+    let toChange = document.querySelector(`#sq${index}`);
+    if (toChange.innerText === '') {
+      numPlays++;
+      gameBoard.gameboard[index] = currPlayer.choice;
+      toChange.innerText = gameBoard.gameboard[index];
+      currPlayer === players[0]
+        ? (currPlayer = players[1])
+        : (currPlayer = players[0]);
+      isGameOver();
+    }
+  };
+
+  const isGameOver = () => {
+    if (numPlays == 9) {
+      gameOver();
+    }
+    checkRows();
+    checkColumns();
+    checkDiagonals();
+  };
+
+  const checkRows = () => {
+    let rows = [];
+    let i = 0;
+    while (i < 9) {
+      let row = [];
+      row.push(gameBoard.gameboard[i]);
+      row.push(gameBoard.gameboard[i + 1]);
+      row.push(gameBoard.gameboard[i + 2]);
+      rows.push(row);
+      i += 3;
+    }
+    rows.forEach((row, i) => {
+      if (row[0] == row[1] && row[0] == row[2]) {
+        if (row[0] !== '') {
+          winMessage = `WIN ON ROW ${i + 1}`;
+          gameOver();
         }
-    }
+      }
+    });
+  };
 
-    const isGameOver = () => {
-        checkRows();
-        checkColumns();
-        checkDiagonals();
+  const checkColumns = () => {
+    let columns = [];
+    let i = 0;
+    while (i < 3) {
+      let column = [];
+      column.push(gameBoard.gameboard[i]);
+      column.push(gameBoard.gameboard[i + 3]);
+      column.push(gameBoard.gameboard[i + 6]);
+      console.log(column);
+      columns.push(column);
+      i += 1;
     }
-
-    const checkRows = () => {
-        let rows = [];
-        let i = 0;
-        while (i < 9) {
-            let row = [];
-            row.push(gameBoard.gameboard[i]);
-            row.push(gameBoard.gameboard[i + 1]);
-            row.push(gameBoard.gameboard[i + 2]);
-            rows.push(row);
-            i += 3;
+    columns.forEach((column, i) => {
+      if (column[0] == column[1] && column[0] == column[2]) {
+        if (column[0] !== '') {
+          winMessage = `WIN ON COLUMN ${i + 1}`;
+          gameOver();
         }
-        rows.forEach((row, i) => {
-            if (row[0] == row[1] && row[0] == row[2]) {
-                if (row[0] !== '') {
-                    console.log(`WIN ON ROW ${i + 1}`);
-                }
-            }
-        });
-    }
+      }
+    });
+  };
 
-    const checkColumns = () => {
-        let columns = [];
-        let i = 0;
-        while (i < 3) {
-            let column = [];
-            column.push(gameBoard.gameboard[i]);
-            column.push(gameBoard.gameboard[i + 3]);
-            column.push(gameBoard.gameboard[i + 6]);
-            console.log(column);
-            columns.push(column);
-            i += 1;
-        }
-        columns.forEach((column, i) => {
-            if (column[0] == column[1] && column[0] == column[2]) {
-                if (column[0] !== '') {
-                    console.log(`WIN ON COLUMN ${i + 1}`);
-                }
-            }
-        });
+  const checkDiagonals = () => {
+    let gb = gameBoard.gameboard;
+    if (gb[4] !== '') {
+      if (gb[0] == gb[4] && gb[0] == gb[8]) {
+        winMessage = `WIN ON 1st DIAGONAL!`;
+        gameOver();
+      }
+      if (gb[2] == gb[4] && gb[2] == gb[6]) {
+        winMessage = 'WIN ON 2nd DIAGONAL!';
+        gameOver();
+      }
     }
+  };
 
-    const checkDiagonals = () => {
-        let gb = gameBoard.gameboard;
-        if (gb[4] !== '') {
-            if (gb[0] == gb[4] && gb[0] == gb[8]) {
-                console.log(`WIN ON 1st DIAGONAL!`);
-            }
-            if (gb[2] == gb[4] && gb[2] == gb[6]) {
-                console.log('WIN ON 2nd DIAGONAL!')
-            }
-        }
-    }
-
-    const gameOver = () => {
-        console.log('boohoo!');
-    }
-
-    return {
-        render,
-    }
+  const gameOver = () => {
+    restartbtn.style.display = 'block';
+  };
+  return {
+    render,
+  };
 })();
 
 gameBoard.setup();
